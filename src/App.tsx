2ad1b2e8db0bbc231d33c82a1962ed59c34e0424
VictorSarov <victor.sarov@rentalmatics.com>
2026-06-01 16:07:14 +0100

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className='flex items-center justify-center h-screen bg-gray-100'>
-            <h1 className='text-3xl font-bold text-blue-500'>Tailwind is working 🚀</h1>
-        </div>
-    );
+    return <h1 className='text-3xl font-bold underline text-red-600'>Hello world!</h1>;
 }
 
 export default App;
