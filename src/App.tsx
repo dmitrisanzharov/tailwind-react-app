1544c6ec0c826311fa350ec309dc89bbac2ae173
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-17 09:31:00 +0100

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
 
 function App() {
-    return (
-        <div>
-            <div className='text-3xl font-bold underline m-5 bg-amber-500'>Hello world!</div>
-        </div>
-    );
+  return (
+  <h1 className="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+  );
 }
 
 export default App;
